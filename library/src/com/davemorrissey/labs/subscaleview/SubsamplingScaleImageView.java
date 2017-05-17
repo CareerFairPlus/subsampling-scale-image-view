68405dbc9dbf03c8bffd3f88fbaac1b288d3ff94
Christopher Buruchian <cj@careerfairplus.com>
2017-05-17 14:48:27 -0400

    Testing quick fix for reference bug

--- library/src/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
+++ library/src/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
@@ -311,8 +311,8 @@ public class SubsamplingScaleImageView extends View {
                     setImage(ImageSource.asset(assetName).tilingEnabled());
                 }
             }
-            if (typedAttr.hasValue(R.styleable.SubsamplingScaleImageView_src)) {
-                int resId = typedAttr.getResourceId(R.styleable.SubsamplingScaleImageView_src, 0);
+            if (typedAttr.hasValue(R.styleable.SubsamplingScaleImageView_srcDrawable)) {
+                int resId = typedAttr.getResourceId(R.styleable.SubsamplingScaleImageView_srcDrawable, 0);
                 if (resId > 0) {
                     setImage(ImageSource.resource(resId).tilingEnabled());
                 }
